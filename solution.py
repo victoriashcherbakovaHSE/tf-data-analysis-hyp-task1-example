d671f6c42f6164adaa561ad7451249640b6c73d6
Виктория Щербакова <96340050+victoriashcherbakovaHSE@users.noreply.github.com>
2023-04-12 13:51:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as st
 
 
 chat_id = 871302863
@@ -8,10 +9,15 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    p1 = x_success / x_cnt
-    p2 = y_success / y_cnt
-    p = (x_success + y_success) / (x_cnt + y_cnt)
     n1 = x_cnt
     n2 = y_cnt
-    z = (p1 - p2) / (p * (1 - p) * (1/n1 + 1/n2)) ** 0.5
-    return abs(z) > 1.96
+    X1 = x_success
+    X2 = y_success
+    p1 = X1 / n1
+    p2 = X2 / n2
+    p_combined = (X1 + X2) / (n1 + n2)
+    E1 = n1 * p_combined
+    E2 = n2 * p_combined
+    chi_squared_stat = ((X1 - E1)**2 / E1) + ((X2 - E2)**2 / E2)
+    p_value = 1 - st.chi2.cdf(chi_squared_stat, 1)
+    return p_value < 0.05
