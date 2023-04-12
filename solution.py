46f6c80692849bc3221cc2e297786c0872d4485e
Виктория Щербакова <96340050+victoriashcherbakovaHSE@users.noreply.github.com>
2023-04-12 12:20:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 871302863
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    n1 = x_cnt
+    n2 = y_cnt
+    z = (p1 - p2) / (p * (1 - p) * (1/n1 + 1/n2)) ** 0.5
+    return abs(z) > 1.96
